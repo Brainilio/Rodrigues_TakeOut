98ef1915e87788ba006c2fd8e5169c6daad86abf
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-07-04 15:08:10 +0200

    created components and turned app to class

--- src/App.js
+++ src/App.js
@@ -1,131 +1,133 @@
-import React from "react"
+import React, { Component } from "react"
 
-function App() {
-	return (
-		<>
-			<div className='landing'>
-				<div className='landing-logo'>
-					<h1>Rodrigues</h1>
-					<span>Takeout</span>
-				</div>
-				<button>
-					<strong>ORDER NOW</strong>
-				</button>
-			</div>
-			<div className='about'>
-				<div className='about-text'>
-					<h1 className='about-text-header'>About Rodrigues</h1>
-					<p className='about-text-paragraph'>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur
-						quasi exercitationem veniam eligendi. Praesentium necessitatibus,
-						repellendus quo magni quisquam a laboriosam eum quaerat sequi
-						maiores, optio eligendi. Minima, tenetur aperiam!
-					</p>
-					<button className='see-menu'>
+class App extends Component {
+	render() {
+		return (
+			<>
+				<div className='landing'>
+					<div className='landing-logo'>
+						<h1>Rodrigues</h1>
+						<span>Takeout</span>
+					</div>
+					<button>
 						<strong>SEE MENU</strong>
 					</button>
 				</div>
-				<div className='picture'></div>
-			</div>
-			<div className='menu-divider'>
-				<h1>MENU</h1>
-			</div>
-			<div className='menu-page'>
-				<div className='about-items'>
-					{/* description */}
-					<div className='wings-description'>
-						<h1 className='wings-description-header'>RODRIGUES' WINGS</h1>
-						<p className='wings-description-paragraph'>
-							Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores
-							quisquam voluptatem voluptates dignissimos saepe incidunt vitae,
-							culpa nostrum earum voluptatum doloremque ab mollitia, deleniti
-							totam optio ipsam facere id recusandae.
+				<div className='about'>
+					<div className='about-text'>
+						<h1 className='about-text-header'>About Rodrigues</h1>
+						<p className='about-text-paragraph'>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur
+							quasi exercitationem veniam eligendi. Praesentium necessitatibus,
+							repellendus quo magni quisquam a laboriosam eum quaerat sequi
+							maiores, optio eligendi. Minima, tenetur aperiam!
 						</p>
+						{/* <button className='see-menu'>
+						<strong>SEE MENU</strong>
+					</button> */}
 					</div>
-					<div className='wings-picture'></div>
+					<div className='picture'></div>
 				</div>
+				<div className='menu-divider'>
+					<h1>MENU</h1>
+				</div>
+				<div className='menu-page'>
+					<div className='about-items'>
+						{/* description */}
+						<div className='wings-description'>
+							<h1 className='wings-description-header'>RODRIGUES' WINGS</h1>
+							<p className='wings-description-paragraph'>
+								Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+								Maiores quisquam voluptatem voluptates dignissimos saepe
+								incidunt vitae, culpa nostrum earum voluptatum doloremque ab
+								mollitia, deleniti totam optio ipsam facere id recusandae.
+							</p>
+						</div>
+						<div className='wings-picture'></div>
+					</div>
 
-				{/* choose your size */}
+					{/* choose your size */}
 
-				<div className='side-divider'>
-					<h1>CHOOSE YOUR SIZE</h1>
-				</div>
+					<div className='side-divider'>
+						<h1>CHOOSE YOUR SIZE</h1>
+					</div>
 
-				{/* size row */}
-				<div className='size-row'>
-					{/* Single item */}
-					<div className='chicken-item-size'>
-						<div className='chicken-item-picture'></div>
-						{/* Single item description  */}
-						<span>Regular</span>
-						<h3>48 Wings</h3>
-						<h1>€25</h1>
+					{/* size row */}
+					<div className='size-row'>
+						{/* Single item */}
+						<div className='chicken-item-size'>
+							<div className='chicken-item-picture'></div>
+							{/* Single item description  */}
+							<span>Regular</span>
+							<h3>48 Wings</h3>
+							<h1>€25</h1>
+						</div>
+						<div className='chicken-item-size'>
+							<div className='chicken-item-picture'></div>
+							{/* Single item description  */}
+							<span>Regular</span>
+							<h3>48 Wings</h3>
+							<h1>€25</h1>
+						</div>
+						<div className='chicken-item-size'>
+							<div className='chicken-item-picture'></div>
+							{/* Single item description  */}
+							<span>Regular</span>
+							<h3>48 Wings</h3>
+							<h1>€25</h1>
+						</div>
 					</div>
-					<div className='chicken-item-size'>
-						<div className='chicken-item-picture'></div>
-						{/* Single item description  */}
-						<span>Regular</span>
-						<h3>48 Wings</h3>
-						<h1>€25</h1>
+					<div className='side-divider'>
+						<h1>KICK UP SOME SAUCE</h1>
 					</div>
-					<div className='chicken-item-size'>
-						<div className='chicken-item-picture'></div>
-						{/* Single item description  */}
-						<span>Regular</span>
-						<h3>48 Wings</h3>
-						<h1>€25</h1>
+					{/* Sauce row */}
+					<div className='size-row'>
+						{/* Single item */}
+						<div className='chicken-item-size'>
+							<div className='chicken-item-picture'></div>
+							{/* Single item description  */}
+							<h3>48 Wings</h3>
+						</div>
+						<div className='chicken-item-size'>
+							<div className='chicken-item-picture'></div>
+							{/* Single item description  */}
+							<h3>48 Wings</h3>
+						</div>
+						<div className='chicken-item-size'>
+							<div className='chicken-item-picture'></div>
+							{/* Single item description  */}
+							<h3>48 Wings</h3>
+						</div>
+						<div className='chicken-item-size'>
+							<div className='chicken-item-picture'></div>
+							{/* Single item description  */}
+							<h3>48 Wings</h3>
+						</div>
+						<div className='chicken-item-size'>
+							<div className='chicken-item-picture'></div>
+							{/* Single item description  */}
+							<h3>48 Wings</h3>
+						</div>
+						<div className='chicken-item-size'>
+							<div className='chicken-item-picture'></div>
+							{/* Single item description  */}
+							<h3>48 Wings</h3>
+						</div>
+					</div>
+					<div className='order-box'>
+						<h1>ORDER NOW</h1>
+						<span className='description-order'>
+							Please order ahead of time! <br /> Questions or ready to order?
+						</span>
+						<i className='fa fa-whatsapp'></i>
+						<span className='telephone-number'>
+							<a href='tel:+31634500512'>+31634500512</a>
+						</span>
 					</div>
 				</div>
-				<div className='side-divider'>
-					<h1>KICK UP SOME SAUCE</h1>
-				</div>
-				{/* Sauce row */}
-				<div className='size-row'>
-					{/* Single item */}
-					<div className='chicken-item-size'>
-						<div className='chicken-item-picture'></div>
-						{/* Single item description  */}
-						<h3>48 Wings</h3>
-					</div>
-					<div className='chicken-item-size'>
-						<div className='chicken-item-picture'></div>
-						{/* Single item description  */}
-						<h3>48 Wings</h3>
-					</div>
-					<div className='chicken-item-size'>
-						<div className='chicken-item-picture'></div>
-						{/* Single item description  */}
-						<h3>48 Wings</h3>
-					</div>
-					<div className='chicken-item-size'>
-						<div className='chicken-item-picture'></div>
-						{/* Single item description  */}
-						<h3>48 Wings</h3>
-					</div>
-					<div className='chicken-item-size'>
-						<div className='chicken-item-picture'></div>
-						{/* Single item description  */}
-						<h3>48 Wings</h3>
-					</div>
-					<div className='chicken-item-size'>
-						<div className='chicken-item-picture'></div>
-						{/* Single item description  */}
-						<h3>48 Wings</h3>
-					</div>
-				</div>
-				<div className='order-box'>
-					<h1>ORDER NOW</h1>
-					<span className='description-order'>
-						Please order ahead of time! <br /> Questions or ready to order?
-					</span>
-					<i className='fa fa-whatsapp'></i>
-					<span className='telephone-number'>
-						<a href='tel:+31634500512'>+31634500512</a>
-					</span>
-				</div>
-			</div>
-		</>
-	)
+			</>
+		)
+	}
 }
 
 export default App
