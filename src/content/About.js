4cf3c5437b17ae596632fa56c84f5563d04eb587
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-07-08 22:37:52 +0200

    updated styling and added pictures

--- src/content/About.js
+++ src/content/About.js
@@ -1,11 +1,12 @@
 import React from "react"
+import ProfilePicture from "../assets/profile.png"
 
 const About = (props) => {
 	return (
-		<div className='about'>
-			<div className='about-text'>
-				<h1 className='about-text-header'>About Rodrigues</h1>
-				<p className='about-text-paragraph'>
+		<div className="about">
+			<div className="about-text">
+				<h1 className="about-text-header">About Rodrigues</h1>
+				<p className="about-text-paragraph">
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur
 					quasi exercitationem veniam eligendi. Praesentium necessitatibus,
 					repellendus quo magni quisquam a laboriosam eum quaerat sequi maiores,
@@ -15,11 +16,7 @@ const About = (props) => {
 						<strong>SEE MENU</strong>
 					</button> */}
 			</div>
-			<img
-				className='picture'
-				alt='brainilio'
-				src='https://via.placeholder.com/50'
-			/>
+			<img className="picture" alt="brainilio" src={ProfilePicture} />
 		</div>
 	)
 }
--- src/content/Landing.js
+++ src/content/Landing.js
@@ -1,16 +1,20 @@
 import React from "react"
+import BackgroundImage from "../assets/background.jpg"
 
 import Button from "../components/Button"
 
 const Landing = (props) => {
 	return (
-		<div className='landing'>
-			<div className='landing-logo'>
-				<h1>Rodrigues</h1>
-				<span>Takeout</span>
+		<>
+			<div className="landing">
+				<img src={BackgroundImage} alt="" />
+				<div className="landing-logo">
+					<h1>Rodrigues</h1>
+					<span>Takeout</span>
+				</div>
+				<Button>SEE MENU</Button>
 			</div>
-			<Button>SEE MENU</Button>
-		</div>
+		</>
 	)
 }
 
